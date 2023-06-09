b81752032f8ce51ef9cb303f49861a14b9ee18be
Konger6 <konger@purdue.edu>
2023-06-09 12:49:55 -0400

    I added some three.js to have a sort of 3d animation in the background. I want to work on having a cityscape, but at first it shows sophie sign billboard in a city. with the lights going on and off. The background is good for now, and I might continue to work on the other components of it.

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 // Smooth scrolling when clicking on links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
     });
-});
+  });
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    // Initialize Three.js
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.querySelector('.canvas-container').appendChild(renderer.domElement);
+  
+    // Create Cityscape Geometry
+    const geometry = new THREE.BoxGeometry(1, 1, 1); // Adjust size as needed
+  
+    // Create Cityscape Material
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Adjust color as needed
+  
+    // Create Cityscape Mesh
+    const cityScape = new THREE.Mesh(geometry, material);
+    scene.add(cityScape);
+  
+    // Set Camera Position
+    camera.position.z = 5;
+  
+    // Render Loop
+    function animate() {
+      requestAnimationFrame(animate);
+      cityScape.rotation.x += 0.01;
+      cityScape.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    }
+    animate();
+  });
+  
