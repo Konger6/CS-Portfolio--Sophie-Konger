80e78dcc077f4bf0c9b553107bc35fd97bc2fcf9
Konger6 <konger@purdue.edu>
2023-06-09 19:19:44 -0400

    Was able to play around with the background, but am going to change it up, and make something in blender instead, I think it will end up looking a lot better and easier. Changed up the content under about me, and added the scrolling bar to look good.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// Smooth scrolling when clicking on links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+window.addEventListener('DOMContentLoaded', () => {
+    // Smooth scrolling when clicking on links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
   
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
       });
     });
-  });
   
-  window.addEventListener('DOMContentLoaded', () => {
     // Initialize Three.js
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -21,10 +21,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     const geometry = new THREE.BoxGeometry(1, 1, 1); // Adjust size as needed
   
     // Create Cityscape Material
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Adjust color as needed
+    const material = new THREE.MeshBasicMaterial({ color: 0x888888 }); // Adjust color as needed
   
-    // Create Cityscape Mesh
-    const cityScape = new THREE.Mesh(geometry, material);
+    // Create Cityscape Meshes
+    const cityScape = new THREE.Group();
+    const buildingsCount = 30; // Adjust the number of buildings
+  
+    for (let i = 0; i < buildingsCount; i++) {
+      const building = new THREE.Mesh(geometry, material);
+      building.position.x = (Math.random() - 0.5) * 10; // Adjust position randomness
+      building.position.y = (Math.random() - 0.5) * 5; // Adjust position randomness
+      building.position.z = i * -2; // Adjust the distance between buildings
+      cityScape.add(building);
+    }
+  
     scene.add(cityScape);
   
     // Set Camera Position
@@ -37,6 +47,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       cityScape.rotation.y += 0.01;
       renderer.render(scene, camera);
     }
+  
     animate();
   });
   
